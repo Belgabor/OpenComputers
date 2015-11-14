f353159227fd7dd621e4fcb863b6fd510ab8e022
Belgabor <belgabor@gmx.de>
2015-11-14 08:54:49 +0100

    Extractor control with min items processed, cvt version

--- extractor-control-cvt.lua
+++ extractor-control-cvt.lua
@@ -1,5 +1,4 @@
 local component = require("component")
-local rs = component.redstone
 local sides = require("sides")
 local shell = require("shell")
 
@@ -12,7 +11,88 @@ local s_act_extractor = sides.right
 local s_rs_clutch = sides.up
 local min_process = 5
 
-local have_ecu = component.isAvailable("EngineControlUnit")
+local speed = 1024
+local torque = 256
+
+local cvts = {}
+local i = 0
+for addr, dummy in component.list("AdvancedGears") do
+  i = i + 1
+  cvts[i] = component.proxy(addr)
+end
+
+local ecus = {}
+i = 0
+for addr, dummy in component.list("EngineControlUnit") do
+  i = i + 1
+  ecus[i] = component.proxy(addr)
+end
+
+local cvt_setting = {}
+
+-- Setting 1, 512 torque
+if (torque < 512) then
+  cvt_setting[1] = -(512/torque)
+else
+  cvt_setting[1] = torque/512
+end
+
+-- Setting 2, 1 torque
+cvt_setting[2] = torque
+
+-- Setting 3, 256 torque
+if (torque < 256) then
+  cvt_setting[3] = -(256/torque)
+else
+  cvt_setting[3] = torque/256
+end
+
+-- Check validity
+local err = false
+for i = 1,3 do
+  local n_cvts = math.ceil(math.log(math.abs(cvt_setting[i]))/math.log(32))
+  local x = "OK"
+  if (n_cvts > #cvts) then
+    if (i==2) then
+      x = "Inefficient ("..cvt_setting[2]..")"
+      cvt_setting[2] = 32^#cvts
+    else
+      x = "ERROR!"
+      err = true
+    end
+  end
+  local s, t
+  if (cvt_setting[i] < 0) then
+    s = -speed / cvt_setting[i]
+    t = -torque*cvt_setting[i]
+  else
+    s = speed * cvt_setting[i]
+    t = torque/cvt_setting[i]
+  end
+  print(i, cvt_setting[i], n_cvts, s, t, x)
+end
+
+if (err) then
+  os.exit()
+end
+
+function setECU(s)
+  for i, e in pairs(ecus) do
+    e.setECU(s)
+  end
+end
+
+function setRatio(r)
+  for i, cvt in pairs(cvts) do
+    if (math.abs(r)>=32) then
+      cvt.setRatio(32 * (r/math.abs(r)))
+      r = r / 32
+    else
+      cvt.setRatio(r)
+      r = 1
+    end
+  end
+end
 
 if (component.isAvailable("transposer")) then
   act = component.transposer
@@ -30,8 +110,8 @@ end
 
 if (#args > 0) then
   if (args[1] == "off") then
-    if (have_ecu) then
-      component.EngineControlUnit.setECU(0)
+    if (#ecus > 0) then
+      setECU(0)
     else
       print("No ECU available!")
     end
@@ -96,16 +176,16 @@ while true do
       old_size = s[current_step]
     end
     
-    if (have_ecu) then
-      component.EngineControlUnit.setECU(4)
+    if (#ecus>0) then
+      setECU(4)
     end
-    rs.setOutput(s_rs_clutch, signal)
+    setRatio(cvt_setting[signal+1])
     os.sleep(1)
   else
     old_size = 0
     old_step = 0
-    if (have_ecu) then
-      component.EngineControlUnit.setECU(0)
+    if (#ecus>0) then
+      setECU(0)
     end
     if (x>0) then
       print((os.time()-x)/72)
